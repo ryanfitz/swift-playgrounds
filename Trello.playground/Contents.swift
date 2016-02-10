83615df796056547f70ff7d3cab1cb00db2863a9
Ryan Fitzgerald <ryan.fitz1@gmail.com>
2016-02-10 15:11:10 -0500

    recursive

--- Trello.playground/Contents.swift
+++ Trello.playground/Contents.swift
@@ -32,6 +32,19 @@ func unhash(hash: Int) -> String {
     return String(result.reverse())
 }
 
+func unhashRecursive(hash: Int, availableLetters: [Character] = Array("acdegilmnoprstuw".characters), acc: String = "") -> String {
+    guard hash > 7 else {
+        return acc
+    }
+    
+    let charIdx = hash % 37
+    let letter = String(availableLetters[charIdx])
+    
+    let nextHash = (hash - charIdx) / 37
+    
+    return unhashRecursive(nextHash, availableLetters: availableLetters, acc: letter + acc)
+}
+
 let h = hash("leepadg")
 unhash(h)
 assert("leepadg" == unhash(h))
@@ -39,3 +52,8 @@ assert("leepadg" == unhash(h))
 let u = unhash(25377615533200)
 hash(u)
 assert(25377615533200 == hash(u))
+
+let ur = unhashRecursive(25377615533200)
+hash(ur)
+assert(25377615533200 == hash(ur))
+
